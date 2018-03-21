63b3891783e3af365a1f6d251fb3bf6ee4f31f9d
Chris Mayer <mayerxc@gmail.com>
2018-03-21 19:07:28 -0400

    finish calendar exercise

--- calendar_exercise.py
+++ calendar_exercise.py
@@ -1,7 +1,14 @@
 import time
+import datetime
+
 
 now = time.asctime(time.localtime(time.time()))
 print(now)
 
 now = time.time()
 print("Tomorrow will be", time.asctime(time.localtime(now + 60 * 60 * 24 )) )
+print("A year ago it was", time.asctime(time.localtime(now - 365 * 60 * 60 * 24 )) )
+print("In 100 hours it will be", time.asctime(time.localtime(now + 60 * 60 * 100 )) )
+
+birthday =  datetime.datetime(1978, 10, 5, 7, 10)
+print("I was born on", birthday.isoformat(" "))
